da5654b041dfa95d41dbcfde1c795e234182e94a
codelion1024 <890700.ok@163.com>
2018-09-24 16:50:27 +0800

    重构通用OTA编译环境
    修改点:版本v3.1
    1 创建1713等hudson任务支持机型的特殊处理脚本, 用于记录和完成对特定ota包的处理
    以后对于要手动改包的任务,只需要在对应脚本里添加处理语句即可.
    该脚本会记录每个机型的所有版本的特殊处理
    测试建议:可以完成OTA编译
    
    Change-Id: I197d9286044c5cbd5d60e28b91b966f95c6f3090
    Signed-off-by: codelion1024 <890700.ok@163.com>

---
+++ extra_script/QK1607/extra_up.sh
@@ -0,0 +1,18 @@
+﻿#!/system/bin/sh
+
+
+srcver=         `cat /tmp/info.txt | grep srcver | busybox cut -d '=' -f 2`
+tgtver=         `cat /tmp/info.txt | grep tgtver | busybox cut -d '=' -f 2`
+device=         `cat /tmp/info.txt | grep device | busybox cut -d '=' -f 2`
+style=          `cat /tmp/info.txt | grep style  | busybox cut -d '=' -f 2`
+SIGNTYPE=       `cat /tmp/info.txt | grep SIGNTYPE  | busybox cut -d '=' -f 2`
+priority=       `cat /tmp/info.txt | grep priority  | busybox cut -d '=' -f 2`
+full_bsp_modem= `cat /tmp/info.txt | grep full_bsp_modem  | busybox cut -d '=' -f 2`
+PLATFORM=       `cat /tmp/info.txt | grep PLATFORM  | busybox cut -d '=' -f 2`
+hw_version=     `cat /tmp/info.txt | grep hw_version  | busybox cut -d '=' -f 2`
+
+if [ $device != $(getprop ro.product.device) ]; then
+    echo "ota package is build for $device, can apply for $(getprop ro.product.device)"
+fi
+
+
---
+++ extra_script/QK1713/extra_full.sh
@@ -0,0 +1,25 @@
+﻿#!/system/bin/sh
+
+
+srcver=         `cat /tmp/info.txt | grep srcver | busybox cut -d '=' -f 2`
+tgtver=         `cat /tmp/info.txt | grep tgtver | busybox cut -d '=' -f 2`
+device=         `cat /tmp/info.txt | grep device | busybox cut -d '=' -f 2`
+style=          `cat /tmp/info.txt | grep style  | busybox cut -d '=' -f 2`
+SIGNTYPE=       `cat /tmp/info.txt | grep SIGNTYPE  | busybox cut -d '=' -f 2`
+priority=       `cat /tmp/info.txt | grep priority  | busybox cut -d '=' -f 2`
+full_bsp_modem= `cat /tmp/info.txt | grep full_bsp_modem  | busybox cut -d '=' -f 2`
+PLATFORM=       `cat /tmp/info.txt | grep PLATFORM  | busybox cut -d '=' -f 2`
+hw_version=     `cat /tmp/info.txt | grep hw_version  | busybox cut -d '=' -f 2`
+
+if [ $device != $(getprop ro.product.device) ]; then
+    echo "ota package is build for $device, can apply for $(getprop ro.product.device)"
+fi
+
+
+# 解决 Bug 188158: 【1713-A01_049】【智能聚合页】48-49OTA后负一屏未删除
+# 解决 Bug 188600: 【1713-A01--052】【OS3.0】一路OTA升级成功后快视频仍然存在
+if [ tgtver == "054" ]; then
+  rm -rf  /data/app/com.qiku.cardmanager*
+  rm -rf  /data/app/com.lightsky.video*
+fi
+    

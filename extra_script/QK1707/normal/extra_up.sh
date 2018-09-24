8a5792f8206d5016d383b12b058a1730aaa62aaf
codelion1024 <890700.ok@163.com>
2018-09-24 17:17:23 +0800

    8.1升级项目OTA环境搭建:适配1707
    修改点:1 8.1升级项目OTA环境搭建:适配1707
    测试建议:无
    
    Change-Id: I6e1e4ac41ef1a8a2eba195ded588ba64ed51ed03
    Signed-off-by: codelion1024 <890700.ok@163.com>

---
+++ extra_script/QK1707/normal/extra_up.sh
@@ -0,0 +1,31 @@
+﻿#!/sbin/sh
+
+srcver=`busybox cat /tmp/info.txt | busybox grep srcver | busybox cut -d '=' -f 2`
+tgtver=`busybox cat /tmp/info.txt | busybox grep tgtver | busybox cut -d '=' -f 2`
+device=`busybox cat /tmp/info.txt | busybox grep device | busybox cut -d '=' -f 2`
+style=`busybox cat /tmp/info.txt | busybox grep style | busybox cut -d '=' -f 2`
+SIGNTYPE=`busybox cat /tmp/info.txt | busybox grep SIGNTYPE | busybox cut -d '=' -f 2`
+priority=`busybox cat /tmp/info.txt | busybox grep priority | busybox cut -d '=' -f 2`
+full_bsp_modem=`busybox cat /tmp/info.txt | busybox grep full_bsp_modem | busybox cut -d '=' -f 2`
+PLATFORM=`busybox cat /tmp/info.txt | busybox grep PLATFORM | busybox cut -d '=' -f 2`
+hw_version=`busybox cat /tmp/info.txt | busybox grep hw_version | busybox cut -d '=' -f 2`
+# use -v '^#' to filter out comment lines
+ver_local=`busybox grep -v '^#' /default.prop | busybox grep ro.build.version.incremental | busybox cut -d '=' -f 2`
+dev_local=`busybox grep -v '^#' /default.prop | busybox grep ro.product.device | busybox cut -d '=' -f 2`
+
+
+if [ $device != $dev_local ]; then
+    echo "this package is build for $device, can't apply for $dev_local"
+    exit
+fi
+if [ $srcver != $ver_local ]; then
+    echo "this package is forward incremental, build form $srcver, can't apply from $ver_local"
+    exit
+fi
+echo "we use this script to record and handle all of the ARTIFICIAL PROCESS for ${device}'s $style ota update"
+echo "ARTIFICIAL PROCESS begin"
+
+
+
+
+echo "ARTIFICIAL PROCESS end"

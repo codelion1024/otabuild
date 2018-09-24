f409a01ecafdef1e4d2e7ce5944c4910b9d39fff
codelion1024 <890700.ok@163.com>
2018-09-24 17:00:56 +0800

    修复extra_script语法错误
    修改点:版本v3.4
    1 修复脚本语法错误
    测试建议:可以完成OTA编译
    
    Change-Id: I55cc49d7d79a1117f946e48b724743fefaccb75d
    Signed-off-by: codelion1024 <890700.ok@163.com>

--- extra_script/QK1713/extra_full.sh
+++ extra_script/QK1713/extra_full.sh
@@ -1,16 +1,16 @@
 ﻿#!/system/bin/sh
 
 # I am sure $srcver variable is useless for full ota
-tgtver=         `cat /tmp/info.txt | grep tgtver | busybox cut -d '=' -f 2`
-device=         `cat /tmp/info.txt | grep device | busybox cut -d '=' -f 2`
-style=          `cat /tmp/info.txt | grep style  | busybox cut -d '=' -f 2`
-SIGNTYPE=       `cat /tmp/info.txt | grep SIGNTYPE  | busybox cut -d '=' -f 2`
-priority=       `cat /tmp/info.txt | grep priority  | busybox cut -d '=' -f 2`
-full_bsp_modem= `cat /tmp/info.txt | grep full_bsp_modem  | busybox cut -d '=' -f 2`
-PLATFORM=       `cat /tmp/info.txt | grep PLATFORM  | busybox cut -d '=' -f 2`
-hw_version=     `cat /tmp/info.txt | grep hw_version  | busybox cut -d '=' -f 2`
-ver_local=      $(getprop ro.build.version.incremental)
-dev_local=      $(getprop ro.product.device)
+tgtver=`cat /tmp/info.txt | grep tgtver | busybox cut -d '=' -f 2`
+device=`cat /tmp/info.txt | grep device | busybox cut -d '=' -f 2`
+style=`cat /tmp/info.txt | grep style  | busybox cut -d '=' -f 2`
+SIGNTYPE=`cat /tmp/info.txt | grep SIGNTYPE  | busybox cut -d '=' -f 2`
+priority=`cat /tmp/info.txt | grep priority  | busybox cut -d '=' -f 2`
+full_bsp_modem=`cat /tmp/info.txt | grep full_bsp_modem  | busybox cut -d '=' -f 2`
+PLATFORM=`cat /tmp/info.txt | grep PLATFORM  | busybox cut -d '=' -f 2`
+hw_version=`cat /tmp/info.txt | grep hw_version  | busybox cut -d '=' -f 2`
+ver_local=$(getprop ro.build.version.incremental)
+dev_local=$(getprop ro.product.device)
 
 
 if [ $device != $dev_local ]; then

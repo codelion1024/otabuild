81c46360603a79e2819ee0f4b63cb6a8f9c7321f
codelion1024 <890700.ok@163.com>
2018-09-24 17:17:38 +0800

    V4.1.5:1803正向升级到063版本后清除指纹数据
    修改点:1 1803正向升级到063版本后清除指纹数据
    2 添加对ro.build.version.incremental在N和O上不一致的兼容处理逻辑
    3 去掉QK1803/normal/extra_full.sh,QK1803/normal/extra_up.sh BOM头
    测试建议:1803正向升级到063版本后指纹数据清除
    
    Change-Id: Ie57e079fc27e36f7163f33c38c3d08fc5f6fab11
    Signed-off-by: codelion1024 <890700.ok@163.com>

--- extra_script/QK1803/normal/extra_up.sh
+++ extra_script/QK1803/normal/extra_up.sh
@@ -1,4 +1,4 @@
-﻿#!/sbin/sh
+#!/sbin/sh
 
 srcver=`busybox cat /tmp/info.txt | busybox grep srcver | busybox cut -d '=' -f 2`
 tgtver=`busybox cat /tmp/info.txt | busybox grep tgtver | busybox cut -d '=' -f 2`
@@ -12,7 +12,14 @@ hw_version=`busybox cat /tmp/info.txt | busybox grep hw_version | busybox cut -d
 # use -v '^#' to filter out comment lines
 ver_local=`busybox grep -v '^#' /default.prop | busybox grep ro.build.version.incremental | busybox cut -d '=' -f 2`
 dev_local=`busybox grep -v '^#' /default.prop | busybox grep ro.product.device | busybox cut -d '=' -f 2`
+sdk_ver=`busybox grep -v '^#' /default.prop | busybox grep ro.build.version.sdk | busybox cut -d '=' -f 2`
 
+# in project with 7.1.1(sdk 25), ro.build.version.incremental is like '023'
+# in project with 8.1.0(sdk 27), ro.build.version.incremental is like '8.1.077.PX.180518.360OS_360OS_QK1807_CN'
+# if we try to let ver_local stands for a pure digital build number, we should parse againg in 8.1.0
+if [ $sdk_ver -eq 27 ]; then
+  ver_local=`echo $ver_local | cut -d '.' -f 3`
+fi
 
 if [ $device != $dev_local ]; then
     echo "this package is build for $device, can't apply for $dev_local"
@@ -25,7 +32,14 @@ fi
 echo "we use this script to record and handle all of the ARTIFICIAL PROCESS for ${device}'s $style ota update"
 echo "ARTIFICIAL PROCESS begin"
 
-
+# TFS_208071:1803正向升级到063版本后清除指纹数据
+if [ $srcver -lt 063 ] && [ $tgtver -eq 063 ]; then
+  mount -v -t ext4 -o max_batch_time=0,commit=1,data=ordered,barrier=1,errors=panic,nodelalloc /dev/block/bootdevice/by-name/userdata /data
+  echo "handle TFS_208071 begin"
+  rm -f /data/system/locksettings.db
+  echo "handle TFS_208071 end"
+  umount /data
+fi
 
 
 echo "ARTIFICIAL PROCESS end"
